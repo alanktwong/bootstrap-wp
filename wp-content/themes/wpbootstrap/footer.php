5ebbff678126afe0276b6d4d0095e6b837d55fea
awong <alan_kt_wong@yahoo.com>
2013-12-30 16:54:30 -0500

    initial commit

---
+++ wp-content/themes/wpbootstrap/footer.php
@@ -0,0 +1,12 @@
+     <hr>
+
+      <footer>
+        <p>&copy; Audrey S. Lee &amp; Jane Hyun 2013</p>
+      </footer>
+
+    </div> <!-- /container -->
+
+    <?php wp_footer(); ?>
+
+  </body>
+</html>
